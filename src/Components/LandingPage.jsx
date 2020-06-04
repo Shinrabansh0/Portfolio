db013e7d8a539c37ca28205308ae299dd1ab212a
Tyler Felps <felp9179@vandals.uidaho.edu>
2020-06-03 18:45:04 -0700

    Added semantic ui

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -1,38 +1,29 @@
-import React, {Component} from 'react';
-import { Grid, Cell } from 'react-mdl';
+import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
+import { Button, Icon } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
-class Landing extends Component{
-    render(){
-        return(
-            <div style ={{width: '100', margin: 'auto'}}>
-                <Grid className="landing-grid">
-                    <Cell col={12}>
-                       <img src = "https://media-exp1.licdn.com/dms/image/C5603AQE_CJozvDZGKQ/profile-displayphoto-shrink_200_200/0?e=1596067200&v=beta&t=f2SeNb5Cs_coG83ZMolKNtDR_NJfBrq7bd4zbNFAE0Y"
-                            alt = "Avatar"
-                            className = "avatar-img"
-                       />
-                       <div className = "banner-text">
-                        <h1>
-                            Full Stack Web Developer
-                        </h1>
-                        <p>
-                            Javascript | React | CSS | HTML | SQL | NodeJS | Git
-                        </p>
-                        <div className ="social-links">
-                            <a href="http://google.com" rel = "noopener noreferrer" target = "_blank">
-                            <i className="fab fa-linkedin-in"></i>
-                            </a>
-
-                            <a href="http://google.com" rel = "noopener noreferrer" target = "_blank">
-                            <i className="fab fa-linkedin-in"></i>
-                            </a>
-                        </div>
-                       </div>
-                    </Cell>
-                </Grid>
+class Landing extends Component {
+  render() {
+    return (
+      <div style={{ width: "100", margin: "auto" }}>
+        <Grid className="landing-grid">
+          <Cell col={12}>
+            <img
+              src="https://media-exp1.licdn.com/dms/image/C5603AQE_CJozvDZGKQ/profile-displayphoto-shrink_200_200/0?e=1596067200&v=beta&t=f2SeNb5Cs_coG83ZMolKNtDR_NJfBrq7bd4zbNFAE0Y"
+              alt="Avatar"
+              className="avatar-img"
+            />
+            <div className="banner-text">
+              <h1>Full Stack Web Developer</h1>
+              <p>Javascript | React | CSS | HTML | SQL | NodeJS | Git</p>
+              <div className="social-links"></div>
             </div>
-        )
-    }
+          </Cell>
+        </Grid>
+      </div>
+    );
+  }
 }
 
 export default Landing;
